102e09853d126dede624a26cd8e4a731673bb695
mamismeda <amamismeda@gmail.com>
2024-01-12 23:02:09 +0400

    math built in functions

--- BUILD_IN_FUNCTIONS/index.php
+++ BUILD_IN_FUNCTIONS/index.php
@@ -8,7 +8,7 @@
 <body>
     <?php  
      
-     $string = "Hello World!";
+     //$string = "Hello World!";
      //echo strlen($string);
      //echo strpos($string, "o");
      //echo str_replace("World!", "Daniel", $string)
@@ -16,6 +16,13 @@
      //echo strtoupper($string);
      //echo substr($string, 2, -2);
      //print_r(explode(" ", $string));
+
+     $number = -5.5;
+     //echo abs($number);
+     //echo round($number);
+     //echo pow(2, 3); //kubi
+     //echo sqrt(2); //fesvi
+     //echo rand(1, 100);
 
 
 
