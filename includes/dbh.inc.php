cc61ee3e8283180a68b04b8cfe5ef5270d2c730d
mamismeda <amamismeda@gmail.com>
2024-01-21 21:39:18 +0400

    pdo method to connect database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,7 +5,8 @@ $dbusername = "root";
 $dbpassword = "";
 
 try {
-    $pdo = 
-} catch () {
-    
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection Failed :" . $e->getMessage();
 }
