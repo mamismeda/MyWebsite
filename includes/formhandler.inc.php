77a6690dbb53515b75c5bf73750d88169254ddf9
mamismeda <amamismeda@gmail.com>
2024-01-24 23:21:48 +0400

    creating post method to add user in database

---
+++ includes/formhandler.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+ }else{
+    header("Location: ../index.php");
+ }
--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-  <?php 
-        
 
-  ?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <h3> Signup</h3>
+
+  <form action="includes/formhandler.inc.php" method="post">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="pwd" placeholder="password">
+    <input type="text" name="email" placeholder="E-Mail">
+    <button>Signup</button>
+  </form>
 
 </body>
+
 </html>
