448d537908ab3987e37110d6465b32286cade3fc
mamismeda <amamismeda@gmail.com>
2024-01-24 23:33:59 +0400

    add user in users table

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -9,10 +9,19 @@
        require_once "dbh.inc.php";
         // SQL CODE TO INSERT DATA IN DATABASE
        $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);";
+
        // PREPARE QUERY TO ADD IN DATABASSE FOR SECURITY REASONS
        $stmt = $pdo->prepare($query);
+
       // ADD DATA IN DATABASE
        $stmt->execute([$username, $pwd, $email]);
+
+       $pdo = null;
+       $stmt = null;
+
+       header("Location: ../index.php");
+
+       die();
     } catch(PDOException $e){
          die("Query failed: " . $e->getMessage());
     }
