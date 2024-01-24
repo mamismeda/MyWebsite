4ef1b0cdf9a9aace2ab97e0a1aecb96477819cce
mamismeda <amamismeda@gmail.com>
2024-01-24 23:30:00 +0400

    adding data

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -4,6 +4,18 @@
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
     $email = $_POST["email"];
+
+    try{
+       require_once "dbh.inc.php";
+        // SQL CODE TO INSERT DATA IN DATABASE
+       $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);";
+       // PREPARE QUERY TO ADD IN DATABASSE FOR SECURITY REASONS
+       $stmt = $pdo->prepare($query);
+      // ADD DATA IN DATABASE
+       $stmt->execute([$username, $pwd, $email]);
+    } catch(PDOException $e){
+         die("Query failed: " . $e->getMessage());
+    }
  }else{
     header("Location: ../index.php");
  }
