28129d9bb3ca630ef702ddb2b2b37e437da264c1
mamismeda <amamismeda@gmail.com>
2024-02-10 20:49:46 +0400

    pdo connection

--- LOGIN/includes/dbh.inc.php
+++ LOGIN/includes/dbh.inc.php
@@ -8,7 +8,9 @@ $dbpassword = '';
 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname" , $dbusername, $dbpassword);
+    //pdo connection with aditional attributes
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
