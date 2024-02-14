1d1d50dc7ffc38bf9832bbccd0e449a7c1c99aba
mamismeda <amamismeda@gmail.com>
2024-02-14 21:02:02 +0400

    regenerate id for cookies safety

--- LOGIN/includes/config_session.inc.php
+++ LOGIN/includes/config_session.inc.php
@@ -10,3 +10,20 @@ session_set_cookie_params([
     'secure' => true,
     'httponly' => true
 ]);
+
+session_start();
+
+if (!isset($_SESSION["last_regeneration"])) {
+    regenerate_session_id();
+} else {
+    $interval = 60 * 30;
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+        regenerate_session_id();
+    }
+}
+
+function regenerate_session_id()
+{
+    session_regenerate_id();
+    $_SESSION["last_regeneration"] = time();
+}
