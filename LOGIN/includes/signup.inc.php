31a33affb9a6a7e9ada97ea193cca6116b262762
mamismeda <amamismeda@gmail.com>
2024-02-14 21:10:13 +0400

    mvc pattern file structure

--- LOGIN/includes/signup.inc.php
+++ LOGIN/includes/signup.inc.php
@@ -1,5 +1,18 @@
 <?php
 
- if () {
-    
+ if ($_SERVER["REQUEST_METHOD"] === "POST") {
+     $username = $_POST["username"];
+     $pwd = $_POST["pwd"];
+     $email = $_POST["email"];
+
+     try {
+        require_once 'dbh.inc.php';
+
+        
+     } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+     }
+ } else {
+    header("Location: ../index.php");
+    die();
  }
