937b024a11983e45c0d94685e903cbd992803001
mamismeda <amamismeda@gmail.com>
2024-02-15 21:55:14 +0400

    selecting data in database

--- LOGIN/includes/signup.inc.php
+++ LOGIN/includes/signup.inc.php
@@ -1,25 +1,25 @@
 <?php
 
- if ($_SERVER["REQUEST_METHOD"] === "POST") {
-     $username = $_POST["username"];
-     $pwd = $_POST["pwd"];
-     $email = $_POST["email"];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+   $username = $_POST["username"];
+   $pwd = $_POST["pwd"];
+   $email = $_POST["email"];
 
-     try {
-        require_once 'dbh.inc.php';
-        require_once ' signup_model.inc.php';
-        require_once ' singup_contr.inc.php';
+   try {
+      require_once 'dbh.inc.php';
+      require_once ' signup_model.inc.php';
+      require_once ' singup_contr.inc.php';
 
-        //Error hanlders 
-        if(is_input_empty($username, $pwd, $email)) {
-
-        }
-        
-
-     } catch (PDOException $e) {
-        die("Query failed: " . $e->getMessage());
-     }
- } else {
-    header("Location: ../index.php");
-    die();
- }
+      //Error hanlders 
+      if (is_input_empty($username, $pwd, $email)) {
+      }
+      if (is_email_invalid($email)) {
+      }
+      
+   } catch (PDOException $e) {
+      die("Query failed: " . $e->getMessage());
+   }
+} else {
+   header("Location: ../index.php");
+   die();
+}
--- LOGIN/includes/signup_model.inc.php
+++ LOGIN/includes/signup_model.inc.php
@@ -2,3 +2,9 @@
 
 declare(strict_types=1);
 
+
+function get_username(object $pdo, string $username) {
+  $query = "SELECT username FROM userse WHERE username = :username;";
+  $stmt = $pdo->prepare($query);
+}
+
