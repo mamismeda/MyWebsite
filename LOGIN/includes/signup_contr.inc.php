b5a8b9d57966d36dc9e4b2afe80881cfec2b0c3e
mamismeda <amamismeda@gmail.com>
2024-02-15 21:45:52 +0400

    creating controller function logic

--- LOGIN/includes/signup_contr.inc.php
+++ LOGIN/includes/signup_contr.inc.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
-function is_input_empty($username, $pwd, $email) {
-   if
+function is_input_empty($username, $pwd, $email)
+{
+   if (empty($username) || empty($pwd) || empty($email)) {
+      return true;
+   } else {
+      return false;
+   }
 }
