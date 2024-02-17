bcaec0a673795d2bdb38e9c9217716551da6cc90
mamismeda <amamismeda@gmail.com>
2024-02-17 11:37:01 +0400

    grab username from database

--- LOGIN/includes/login.inc.php
+++ LOGIN/includes/login.inc.php
@@ -5,7 +5,33 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $pwd = $_POST["pwd"];
 
     try {
-        
+        require_once 'dbh.inc.php';
+        require_once 'login_model.inc.php';
+        require_once 'login_contr.inc.php';
+
+            //Error hanlders 
+      $errors = [];
+
+      if (is_input_empty($username, $pwd, $email)) {
+         $errors["empty_input"] = "fill in all fields!";
+      }
+
+      require_once 'config_session.inc.php';
+
+      if ($errors) {
+         $_SESSION['errors_signup'] = $errors;
+
+         $signupData = [
+            "username" => $username,
+            "email" => $email
+         ];
+      $_SESSION['signup_data'] = $signupData;
+
+         header("Location : ../index.php");
+         die();
+      }
+
+
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
---
+++ LOGIN/includes/login_model.inc.php
@@ -0,0 +1,7 @@
+<?php
+ 
+declare(strict_types = 1);
+
+ function get_user() {
+    
+ }
