0bb4052e2cac47ac95213fdb6e4f481ff4d7a88b
mamismeda <amamismeda@gmail.com>
2024-02-23 21:05:03 +0400

    creating object copys with class blueprint

--- OOP/Classes/Car.php
+++ OOP/Classes/Car.php
@@ -1,5 +1,19 @@
 <?php
 
-class Car {
-    
+class Car
+{
+    // Properties / Fields
+    private $brand;
+    private $color;
+    private $vehicleType = "car";
+
+    // Contsructor
+    public function __construct($brand, $color = "none")
+    {
+        $this->brand = $brand;
+        $this->color = $color;
+    }
 }
+
+$car01 = new Car("Volvo", "Green");
+$car02 = new Car("BMW");
