3f56f776031017d179a07567554af43bde08c2f4
mamismeda <amamismeda@gmail.com>
2024-02-24 19:26:50 +0400

    creating method

--- OOP/Classes/Car.php
+++ OOP/Classes/Car.php
@@ -5,7 +5,7 @@ class Car
     // Properties / Fields
     private $brand;
     private $color;
-    private $vehicleType = "car";
+    public $vehicleType = "car";
 
     // Contsructor
     public function __construct($brand, $color = "none")
@@ -13,7 +13,11 @@ class Car
         $this->brand = $brand;
         $this->color = $color;
     }
+
+    // Method
+    
 }
 
 $car01 = new Car("Volvo", "Green");
-$car02 = new Car("BMW");
+
+
